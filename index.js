f69011ce182469efff85cfb6ffa5d31bd73000fc
Sagrika Barge <sagrikabarge@gmail.com>
2021-10-03 18:45:20 +0530

    extended: robotask

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var fs = require("fs");
-var superagent = require("superagent");
+const fs = require("fs");
+const superagent = require("superagent");
 
 // Function to generate random string
 
@@ -16,27 +16,36 @@ function randomString(length) {
     return result;
 }
 
-//async function
-// async function getRandomPic() {
-//     try {
-//       const res = await superagent.get(`https://robohash.org/${randomString(4)}`)
-//       console.log('Random image is ', res.request.url)
-//       await writeFilePromise('./randomImage.txt', res.request.url)
-//       console.log('sucessfully written the file')
-//     } catch (err) {
-//       throw err
-//     }
-//     console.log('2. complete')
-//   }
-//   console.log('1. start')
-//   ;(async () => {
-//     try {
-//       await getRandomPic()
-//       console.log('3. end')
-//     } catch (err) {
-//       console.log('3. end due to error')
-//     }
-//   })()
+// async function
+
+async function getRandomPic() {
+    try {
+        const res1 = await superagent.get(`https://robohash.org/${randomString(6)}`)
+        console.log('Random image is ', res1.request.url)
+        const res2 = await superagent.get(`https://robohash.org/${randomString(6)}`)
+        console.log('Random image is ', res2.request.url)
+        const res3 = await superagent.get(`https://robohash.org/${randomString(6)}`)
+        console.log('Random image is ', res3.request.url)
+
+        const all = await Promise.all([res1, res2, res3])
+        const images = all.map((el) => el.request.url)
+        console.log(images);
+        await writeFilePromise('./randomImage.txt', images.join("\n"))
+        console.log('sucessfully written the file')
+    } catch (err) {
+      throw err
+    }
+    console.log('2. complete')
+  }
+  console.log('1. start')
+  ;(async () => {
+    try {
+      await getRandomPic()
+      console.log('3. end')
+    } catch (err) {
+      console.log('3. end due to error')
+    }
+  })()
 
 
 //Promises
@@ -63,16 +72,16 @@ function randomString(length) {
 // })
 
 
-// //Callbacks
-superagent
-    .get(`https://robohash.org/${randomString(5)}`)
-    .end((err, res) => {
-        console.log(res.request.url);
-        fs.writeFile("./randomImage.txt", res.request.url, (err) => {
-            if (err) {
-                console.log("Not able to write image in txt file", err);
-                return;
-            }
-            console.log('Random Image successfully written inside the file');
-        });
-    });
+// Callbacks
+// superagent
+//     .get(`https://robohash.org/${randomString(5)}`)
+//     .end((err, res) => {
+//         console.log(res.request.url);
+//         fs.writeFile("./randomImage.txt", res.request.url, (err) => {
+//             if (err) {
+//                 console.log("Not able to write image in txt file", err);
+//                 return;
+//             }
+//             console.log('Random Image successfully written inside the file');
+//         });
+//     });
